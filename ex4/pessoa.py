2682ea9ac501388e338a4b38875748e806ea16da
lucasmanesco <lucas_manesco@hotmail.com>
2023-03-31 19:23:41 -0300

    exercicio classe pessoa

--- ex4/pessoa.py
+++ ex4/pessoa.py
@@ -4,3 +4,35 @@ class Pessoa:
         self.idade = idade
         self.peso = peso
         self.altura = altura
+
+    def envelhecer(self, envelheceu: int):
+        self.idade += envelheceu
+        if self.idade < 21:
+            self.crescer(0.5)
+
+    def engordar(self, engordou: float):
+        self.peso += engordou
+
+    def emagrecer(self, emagreceu: float):
+        self.peso -= emagreceu
+
+    def crescer(self, crescimento: float):
+        self.altura += crescimento
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.nome!r}, {self.idade!r}, '\
+            f'{self.peso!r}, {self.altura!r})'
+        return f'{class_name}{attrs}'
+
+
+if __name__ == '__main__':
+    p1 = Pessoa('Lucas', 28, 68.5, 1.75)
+    p1.engordar(3)
+    print(p1)
+    p1.envelhecer(2)
+    print(p1)
+    p1.crescer(0.1)
+    print(p1)
+    p1.emagrecer(1)
+    print(p1)
